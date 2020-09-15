9881fa02d3c08e41ced201f56849af4e5b5af048
Marton Hun <hunmarton@hotmail.com>
2020-09-15 15:31:39 +0200

    mobile menu working

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const list = document.getElementById("menu-links");
 const header = document.getElementById("header");
 
 icon.addEventListener("click", () => {
-  console.log(123);
   list.classList.toggle("activate");
   header.classList.toggle("header-activate");
 });
